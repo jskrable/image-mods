34cfa41a1a2a498bdeae8b6d2425a9e6a66224ff
Jack Skrable <jskrable@bu.edu>
2021-01-15 09:57:19 -0500

    updated magick script

--- magick.sh
+++ magick.sh
@@ -12,3 +12,15 @@ composite Downloads/triple-word-score.jpg -gravity center -compose Divide Downlo
 composite Downloads/triple-word-score.jpg -gravity center -compose Lighten Downloads/strategize.jpg Downloads/lighten.jpg
 composite triple-word-score.jpg -gravity center -compose Screen strategize.jpg screen.jpg
 composite triple-word-score.jpg -gravity center -compose bumpmap strategize.jpg bumpmap.jpg
+
+options=(Divide_Dst Divide_Src Plus Minus Minus_Dst Minus_Src Bumpmap Divide Lighten Multiply Screen)
+read -p "Enter image 1: " image1
+read -p "Enter image 2: " image2
+read -p "Enter output directory: " outdir
+mkdir $outdir
+# image1=
+# image2=
+# outdir=
+for i in "${options[@]}"; 
+    do composite $image1 -gravity center -compose $i $image2 "$outdir/$i.jpg";
+done;
